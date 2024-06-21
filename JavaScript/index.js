be2aaec66730151f834fa08910d61ea739af7d99
Sunny Le Bunz <msafinch@gmail.com>
2024-06-21 16:08:33 -0400

    notes for JS added

---
+++ JavaScript/index.js
@@ -0,0 +1,49 @@
+// console.log('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+
+// Variables
+// var, let, const
+
+// Data Types
+// String, Numbers, Boolean, Null, Undefined
+
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof );
+
+// const name = 'John';
+// const age = 30;
+
+// Concatenation
+
+// Below is the older way. 
+// console.log('My name is ' + name + ' and I am '+ age);
+
+// Template String - newer way
+// console.log(`My name is ${name} and I am ${age}`);
+// const hello = (`My name is ${name} and I am ${age}`);
+// console.log(hello);
+
+const s = 'Hello World!';
+
+// Getting the length
+console.log(s.length);
+// Upper Casing
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.split(''));
+
+const p = 'technology, computers, id, code';
+
+console.log(p.split(', '));
+
